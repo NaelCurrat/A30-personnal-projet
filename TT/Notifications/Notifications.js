3a16eca4a8e0d5d5eb905170c298f7c4b1ac05a9
NaelCurrat <nael.currat@gmail.com>
2024-05-14 08:17:33 +0200

    TT

---
+++ TT/Notifications/Notifications.js
@@ -0,0 +1,42 @@
+var $status;
+
+window.addEventListener('load', function() {
+  alert('Page complètement chargée');
+  $status = document.getElementById('status');
+});
+
+if ('Notification' in window) {
+  $status.innerText = Notification.permission;
+}
+
+function requestPermission() {
+  if (!('Notification' in window)) {
+    alert('Notification API not supported!');
+    return;
+  }
+  
+  Notification.requestPermission(function (result) {
+    $status.innerText = result;
+  });
+}
+
+function nonPersistentNotification() {
+  if (!('Notification' in window)) {
+    alert('Notification API not supported!');
+    return;
+  }
+  
+  try {
+    var notification = new Notification("Hi there - non-persistent!");
+  } catch (err) {
+    alert('Notification API error: ' + err);
+  }
+}
+
+function persistentNotification() {
+  if (!('Notification' in window) || !('ServiceWorkerRegistration' in window)) {
+    alert('Persistent Notification API not supported!');
+    return;
+  }
+  
+}
---
+++ TT/OfflineMode/sw.js
@@ -0,0 +1,46 @@
+// Nom du cache
+const cacheName = 'v1';
+
+// Fichiers à mettre en cache
+const cacheAssets = [
+  'index.html',
+  'app.js',
+  'favicon.png'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('Service Worker: Mise en cache des fichiers');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activation du Service Worker
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Activé');
+  // Nettoyage des anciens caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: Nettoyage du cache ancien');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Récupération des requêtes fetch
+self.addEventListener('fetch', e => {
+  console.log('Service Worker: Récupération');
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
